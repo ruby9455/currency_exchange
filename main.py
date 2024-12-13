00dabeb129a41b2f6f2309b686e1ce2bd1ac69e8
Ruby Chan <a1865524@adelaide.edu.au>
2024-12-13 19:28:55 +0800

    update the calculateion

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import streamlit as st
 
+st.title("港幣兌換澳元")
 st.number_input("港幣", value=200000, step=1, key="hkd")
 l, r = st.columns(2)
 l.number_input("賣出價1", value=4.9695, key="selling_price1")
@@ -13,8 +14,8 @@ st.number_input("定期日數", value=7, step=1, key="days")
 l, r = st.columns(2)
 l.number_input("利率1", value=0.15, step=0.01, key="interest_rate1")
 r.number_input("利率2", value=12.8, step=0.01, key="interest_rate2")
-interest1 = round(aud1*st.session_state.interest_rate1*(st.session_state.days/365), 4)
-interest2 = round(aud2*st.session_state.interest_rate2*(st.session_state.days/365), 4)
+interest1 = round(aud1*(st.session_state.interest_rate1/100)*(st.session_state.days/365), 4)
+interest2 = round(aud2*(st.session_state.interest_rate2/100)*(st.session_state.days/365), 4)
 l.write(f"獲得利息1: {interest1}")
 r.write(f"獲得利息2: {interest2}")
 l.write(f"總數1: {round(aud1+interest1, 4)}")
