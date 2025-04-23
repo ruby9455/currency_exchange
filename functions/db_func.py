44421b045ac241a5876ff542d85f83d22e585baa
rubyhnchan <Ruby.Chan2@sa.gov.au>
2025-04-23 22:55:59 +0800

    refactor database functions for improved clarity and add new data retrieval methods

--- functions/db_func.py
+++ functions/db_func.py
@@ -1,8 +1,10 @@
+from pymongo.collection import Collection
+from pymongo.database import Database
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
-from functions.shared_func import _read_toml
+from functions.auth_func import _read_toml
     
-def get_mongo_connection_string() -> str:
+def _get_mongo_connection_string() -> str:
     """
     Get the MongoDB connection string from the TOML file.
     """
@@ -20,7 +22,7 @@ def get_mongo_client() -> MongoClient | None:
     """
     Create a MongoDB client using the connection string from environment variables.
     """
-    connection_str = get_mongo_connection_string()
+    connection_str = _get_mongo_connection_string()
     
     # Create a MongoDB client
     client = MongoClient(connection_str, server_api=ServerApi('1'))
@@ -34,7 +36,7 @@ def get_mongo_client() -> MongoClient | None:
     # Return the client
     return client
 
-def create_db(client: MongoClient, db_name: str) -> None:
+def create_db(client: MongoClient, db_name: str) -> None: # TODO: test the function
     """
     Create a new database in MongoDB.
     """
@@ -42,7 +44,8 @@ def create_db(client: MongoClient, db_name: str) -> None:
     db = client[db_name]
     print(f"Database '{db_name}' created successfully.")
     return db
-def create_collection(db, collection_name: str) -> None:
+
+def create_collection(db, collection_name: str) -> None: # TODO: test the function
     """
     Create a new collection in the specified database.
     """
@@ -50,6 +53,7 @@ def create_collection(db, collection_name: str) -> None:
     collection = db[collection_name]
     print(f"Collection '{collection_name}' created successfully.")
     return collection
+
 def insert_data(collection, data: dict) -> None:
     """
     Insert data into the specified collection.
@@ -58,6 +62,7 @@ def insert_data(collection, data: dict) -> None:
     result = collection.insert_one(data)
     print(f"Data inserted with ID: {result.inserted_id}")
     return result
+
 def fetch_data(collection, query: dict) -> list:
     """
     Fetch data from the specified collection based on the query.
@@ -65,6 +70,7 @@ def fetch_data(collection, query: dict) -> list:
     # Fetch data from the collection
     data = collection.find(query)
     return list(data)
+
 def update_data(collection, query: dict, new_values: dict) -> None:
     """
     Update data in the specified collection based on the query.
@@ -76,7 +82,8 @@ def update_data(collection, query: dict, new_values: dict) -> None:
     else:
         print("No documents matched the query. No data updated.")
     return result
-def delete_data(collection, query: dict) -> None:
+
+def delete_data(collection, query: dict) -> None: # TODO: test the function
     """
     Delete data from the specified collection based on the query.
     """
@@ -87,4 +94,38 @@ def delete_data(collection, query: dict) -> None:
     else:
         print("No documents matched the query. No data deleted.")
     return result
-    
+
+def get_all_databases(client: MongoClient) -> list:
+    """
+    Get a list of all databases in the MongoDB server.
+    """
+    # Get a list of all databases
+    databases = client.list_database_names()
+    return databases
+
+def get_all_collections(client: MongoClient, db_name: str) -> list:
+    """
+    Get a list of all collections in the specified database.
+    """
+    # Get a list of all collections
+    db = client[db_name]
+    collections = db.list_collection_names()
+    return collections
+
+def get_collection(client: MongoClient, db_name: str, collection_name: str) -> Collection:
+    """
+    Get a specific collection from the specified database.
+    """
+    # Get the specified collection
+    db = client[db_name]
+    collection = db[collection_name]
+    return collection
+
+def get_collection_data(client: MongoClient, db_name: str, collection_name: str) -> list:
+    """
+    Get all data from the specified collection.
+    """
+    collection = get_collection(client, db_name, collection_name)
+    # Get all data from the collection
+    data = collection.find()
+    return list(data)
