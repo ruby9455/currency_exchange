06cf93bc28330b0e6f102c18bfc34ada9f7c17da
rubyhnchan <Ruby.Chan2@sa.gov.au>
2025-04-26 10:40:25 +0800

    Refactor transaction history page to improve data handling and UI presentation; add data retrieval and display logic.

--- app/pages/transaction_history.py
+++ app/pages/transaction_history.py
@@ -1,10 +1,28 @@
 import streamlit as st
 from components.navigation import nav_bar
+from functions.utils import _get_default_db, _get_default_collection
 from functions.ui.auth_page import init_auth_state, login_page, auth_header
 
 def transaction_history_page():
-    """Render the transaction history page"""
-    st.title("Transaction History")
+    from functions.ui.db_func_ui import get_collection_data_wrapper
+    default_db = _get_default_db()
+    default_collection = _get_default_collection()
+    st.write(default_db, default_collection)
+    data = get_collection_data_wrapper(db_name=default_db, collection_name=default_collection)
+    if data is not None and not data.empty:
+        st.subheader(body="Transaction History")
+        data = data.drop(columns=["_id", "_created_at", "_updated_at"], errors="ignore")
+        data = data.rename(columns={col: col.replace("_", " ").strip().title() for col in data.columns})
+        data = data.sort_values(by="Date", ascending=False)
+        if len(data.columns) > 2:
+            data = data[[*data.columns[2:], *data.columns[:2]]]
+        st.dataframe(
+            data=data,
+            use_container_width=True,
+            hide_index=True,
+        )
+    else:
+        st.warning(body="No transaction history available.")
 
 def main():
     init_auth_state()
